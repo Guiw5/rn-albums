a4304ae3a92b3ec81a08f40af561e9246bb8abbc
Guido Wagner <wagner.guido.5@gmail.com>
2020-04-15 23:58:45 -0300

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [ 
+    "airbnb", 
+    "airbnb/hooks", 
+    "eslint:recommended", 
+    "plugin:react/recommended", 
+    "plugin:prettier/recommended",
+    "prettier",
+    "prettier/babel",
+    "prettier/flowtype",
+    "prettier/react",
+    "prettier/standard",
+    "@react-native-community",
+  ],
+  parser: "babel-eslint",
+  rules: {
+    "react/jsx-filename-extension": [ 1, { "extensions": [ ".js", ".jsx" , ".ts"] } ],
+    'react/prop-types': [2],
+  },
+  env: {
+    "es6": true,
+    "jest": true,
+  }
+};
